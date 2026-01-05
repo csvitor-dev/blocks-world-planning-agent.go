d76a30a4f230ac33f41ac39ea7edbed036f27f95
csvitor-dev <csvitor.dev@gmail.com>
2026-01-05 10:35:09 -0300

    refactor: update entrypoint

--- cmd/planner/main.go
+++ cmd/planner/main.go
@@ -1,20 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"time"
 
 	"github.com/csvitor-dev/blocks-world-planning-agent.go/internal/domain"
 	"github.com/csvitor-dev/blocks-world-planning-agent.go/src/services/parser"
+	"github.com/csvitor-dev/blocks-world-planning-agent.go/src/support/factories"
+	"github.com/csvitor-dev/blocks-world-planning-agent.go/utils/cmd"
 )
 
-func main() {
-	instance := parser.GetInstance("4-0")
-	planning, err := domain.NewPlanning(instance, "4-0")
+func execute(ref, algorithm string, dontShowOutput bool) {
+	stripsInstance := parser.GetInstance(ref)
+	planning, err := domain.NewPlanning(stripsInstance, ref)
 
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("Error while creating planning for instance %s: %s", ref, err)
+	}
+
+	if dontShowOutput {
+		planning.OffReport()
+	}
+	planner := factories.MakeAlgorithm(algorithm)
+
+	if planner == nil {
+		log.Fatalf("Algorithm %s not found for instance %s", algorithm, ref)
+	}
+	planning.SetAlgorithm(planner(planning))
+
+	if err != nil {
+		log.Fatalf("Error while setting algorithm for instance %s: %s", ref, err)
+	}
+	err = planning.Plan()
+
+	if err != nil {
+		log.Fatalf("Error while planning for instance %s: %s", ref, err)
+	}
+}
+
+func main() {
+	flags, err := cmd.PluckFlagsFromArgs("instance", "algorithm", "only-execute")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	instances := flags["instance"].([]string)
+	algorithm := flags["algorithm"].(string)
+	onlyExecute := flags["only-execute"].(bool)
+
+	if len(instances) > 0 {
+		execute(instances[0], algorithm, onlyExecute)
 		return
 	}
 
-	fmt.Println(planning)
+	for _, ref := range instances {
+		execute(ref, algorithm, onlyExecute)
+		time.Sleep(time.Second * 3)
+	}
 }
